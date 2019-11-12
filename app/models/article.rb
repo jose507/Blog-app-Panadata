96a77c81f7b6fc1fdca1a54b02c60f569e7e6b02
gustavodavidp <gustavo.pol21@gmail.com>
2019-11-11 19:55:12 -0500

    agregando user id a la tabla articulos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    validates :user_id, presence: true
     has_many :comments, dependent: :destroy
     validates :title, presence: true,
                       length: { minimum: 5 }

1b0b33e24160d45218abcad21f509d73ea3d0394
Ivan Barria <ibarria0@gmail.com>
2019-11-13 13:22:53 -0500

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,7 @@ class ArticlesController < ApplicationController
       end
 
     def create
-        @article = Article.new(article_params)
-        @article.user = User.first
+        @article = current_user.articles.new(article_params)
        
         if @article.save
         redirect_to @article
